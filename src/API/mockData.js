1b0db1507d209da2567093bb7cb2c6ef9d8bd3de
jasonburnell98 <jasonburnell666@gmail.com>
2023-01-10 14:27:24 -0500

    rewards

---
+++ src/API/mockData.js
@@ -0,0 +1,92 @@
+export const data = [
+  {
+    id: 1,
+    name: "Jason",
+    transactions: {
+      firstMonth: 91,
+      secondMonth: 120,
+      thirdMonth: 140,
+    },
+  },
+  {
+    id: 2,
+    name: "Mark",
+    transactions: {
+      firstMonth: 122,
+      secondMonth: 170,
+      thirdMonth: 54,
+    },
+  },
+  {
+    id: 3,
+    name: "Steven",
+    transactions: {
+      firstMonth: 43,
+      secondMonth: 192,
+      thirdMonth: 91,
+    },
+  },
+  {
+    id: 4,
+    name: "Tomas",
+    transactions: {
+      firstMonth: 212,
+      secondMonth: 432,
+      thirdMonth: 43,
+    },
+  },
+  {
+    id: 5,
+    name: "Haroon",
+    transactions: {
+      firstMonth: 49,
+      secondMonth: 65,
+      thirdMonth: 90,
+    },
+  },
+  {
+    id: 6,
+    name: "Richard",
+    transactions: {
+      firstMonth: 343,
+      secondMonth: 45,
+      thirdMonth: 78,
+    },
+  },
+  {
+    id: 7,
+    name: "Lawrence",
+    transactions: {
+      firstMonth: 342,
+      secondMonth: 343,
+      thirdMonth: 34,
+    },
+  },
+  {
+    id: 8,
+    name: "Michael",
+    transactions: {
+      firstMonth: 54,
+      secondMonth: 232,
+      thirdMonth: 43,
+    },
+  },
+  {
+    id: 9,
+    name: "Austin",
+    transactions: {
+      firstMonth: 24,
+      secondMonth: 43,
+      thirdMonth: 65,
+    },
+  },
+  {
+    id: 10,
+    name: "Martin",
+    transactions: {
+      firstMonth: 76,
+      secondMonth: 89,
+      thirdMonth: 439,
+    },
+  },
+];
